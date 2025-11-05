4662d13f98a7aa2f06e43c182a68e81a43bc1269
Andre Aloise <aaloise@gmail.com>
2025-11-04 20:59:33 -0400

    Desafio 1

--- desafios.py
+++ desafios.py
@@ -9,12 +9,11 @@ tarefas = []
 
 # Desafio 01: Adicionar uma nova tarefa
 def adicionar_tarefa(titulo):
-    """
-    Adiciona uma nova tarefa à lista.
-    Dica: use append() para inserir o título na lista 'tarefas'.
-    """
-    # TODO: implemente aqui lógica de adicionar tarefa
-    pass
+    if titulo:
+        tarefas.append(titulo)
+        print(f"Tarefa '{titulo}' adicionada com sucesso!")
+    else:
+        print("Erro: O título não pode ser vazio.")
 
 
 # Desafio 02: Listar todas as tarefas
