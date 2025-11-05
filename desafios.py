4ed1d85f89dfb9f4f3f314baf3f56f84d430ff89
lucymary-am <lucymary.am@gmail.com>
2025-11-04 21:10:58 -0400

    listar tarefas

--- desafios.py
+++ desafios.py
@@ -26,9 +26,12 @@ def listar_tarefas():
     Exibe todas as tarefas da lista numeradas.
     Dica: use um for com enumerate() para mostrar o índice e o nome.
     """
-    # TODO: implementar lógica de listagem
-    pass
-
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada.")
+    else:
+        print("\nLista de tarefas:")
+        for i, tarefa in enumerate(tarefas, start=1):
+            print(f"{i}. {tarefa}")
 
 # Desafio 03: Marcar uma tarefa como concluída
 def concluir_tarefa(indice):
