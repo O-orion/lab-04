78365bc7aadcc1a6a634af67fb947a3a13b86636
biinahc <sabrinacaldasads@gmail.com>
2025-11-07 19:24:24 -0400

    subindo desafio 5 concluido Buscar tarega

--- desafios.py
+++ desafios.py
@@ -27,13 +27,11 @@ def remover_tarefa(indice):
 
 # Desafio 05: Buscar tarefa pelo nome
 def buscar_tarefa(nome):
-    """
-    Busca uma tarefa pelo nome e retorna se ela existe ou não.
-    Dica: use um loop para percorrer a lista e comparar strings.
-    """
-    # TODO: implementar lógica de busca
-    pass
-
+    for t in tarefas:
+        if nome in t:
+            print("Tarefa encontrada:", t)
+            return
+    print("Tarefa não encontrada.")
 
 # Desafio 06: Menu interativo (opcional)
 def menu():
